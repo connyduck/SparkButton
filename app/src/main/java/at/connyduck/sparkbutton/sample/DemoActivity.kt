23435f88035442694abbcdd7f88aa80c956b56a6
Conny Duck <git@connyduck.at>
2025-10-06 20:20:13 +0200

    fix deprecations in the demo app

--- app/src/main/java/at/connyduck/sparkbutton/sample/DemoActivity.kt
+++ app/src/main/java/at/connyduck/sparkbutton/sample/DemoActivity.kt
@@ -17,13 +17,14 @@ package at.connyduck.sparkbutton.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -40,6 +41,7 @@ class DemoActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             var selectedTab by remember {
                 mutableIntStateOf(0)
@@ -60,7 +62,7 @@ class DemoActivity : ComponentActivity() {
                     },
                 ) { innerPadding ->
                     Column {
-                        TabRow(
+                        PrimaryTabRow(
                             selectedTabIndex = selectedTab,
                             containerColor = MaterialTheme.colorScheme.primaryContainer,
                             contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
